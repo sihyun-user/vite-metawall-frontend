4bec9735acc43e762a26ab33a9941d56b78fdaef
chsiyu <chsiyu8652@gmail.com>
2022-05-27 17:29:31 +0800

    git init

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  },
+});
+
+export default router;
